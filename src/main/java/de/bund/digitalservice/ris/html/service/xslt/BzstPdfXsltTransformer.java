eef64e5773a46f32cb653e7917edf96ea0cbed14
Weitspringer <luca.springer@posteo.de>
2026-09-25 16:03:07 +0200

    RISDEV-12563 Add Javadoc

--- src/main/java/de/bund/digitalservice/ris/html/service/xslt/BzstPdfXsltTransformer.java
+++ src/main/java/de/bund/digitalservice/ris/html/service/xslt/BzstPdfXsltTransformer.java
@@ -5,10 +5,16 @@ import java.util.Collections;
 /** Class for transforming LegalDocML BZSt documents to HTML-for-PDF using XSLT. */
 public class BzstPdfXsltTransformer extends XsltTransformer {
 
+  /** Transformer XML -> PDF */
   public BzstPdfXsltTransformer() {
     super("de/bund/digitalservice/ris/html/xslt/", "bzst-pdf.xslt");
   }
 
+  /**
+   * Transforms XML bytes to PDF
+   * @param source XML byte source
+   * @return String representing transformed HTML
+   */
   public String transform(byte[] source) {
     return transformLegalDocMlFromBytes(source, Collections.emptyMap());
   }
--- src/main/java/de/bund/digitalservice/ris/html/service/xslt/BzstXsltTransformer.java
+++ src/main/java/de/bund/digitalservice/ris/html/service/xslt/BzstXsltTransformer.java
@@ -5,10 +5,16 @@ import java.util.Collections;
 /** Class for transforming LegalDocML BZSt documents to HTML using XSLT. */
 public class BzstXsltTransformer extends XsltTransformer {
 
+  /** Transformer XML -> HTML */
   public BzstXsltTransformer() {
     super("de/bund/digitalservice/ris/html/xslt/", "bzst.xslt");
   }
 
+  /**
+   * Transforms XML bytes to HTML
+   * @param source XML byte source
+   * @return String representing transformed HTML
+   */
   public String transform(byte[] source) {
     return transformLegalDocMlFromBytes(source, Collections.emptyMap());
   }
